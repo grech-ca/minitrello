4e8fe2b949feff25f6c8919381e5e80bc671487b
Mikhail Grechka <grech.code@gmail.com>
2022-06-29 17:57:45 +0300

    Setup project

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+import { Global, css } from '@emotion/react';
+
+export const GlobalStyles: FC = () => (
+  <Global
+    styles={css({
+      html: {
+        fontFamily: 'Roboto',
+      },
+    })}
+  />
+);
